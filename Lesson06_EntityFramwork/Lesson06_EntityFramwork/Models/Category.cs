c29ef4b3511946a0a6912dc36c3bcc9c3527241e
pghuy <huygiaphi123@gmail.com>
2026-09-24 21:27:38 +0700

    Lesson06_2

---
+++ Lesson06_EntityFramwork/Lesson06_EntityFramwork/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson06_EntityFramwork.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? CategoryName { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
---
+++ Lesson06_EntityFramwork/Lesson06_EntityFramwork/Program.cs
@@ -0,0 +1,45 @@
+﻿using Lesson06_EntityFramwork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson06_EntityFramwork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Đăng ký DbContext với chuỗi kết nối từ appsettings.json để sử dụng Entity Framework Core
+            var connectionString = builder.Configuration.GetConnectionString("BookStoreConn");
+            builder.Services.AddDbContext<BookStoreContext>(options =>
+                options.UseSqlServer(connectionString));
+            
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            app.Run();
+        }
+    }
+}
